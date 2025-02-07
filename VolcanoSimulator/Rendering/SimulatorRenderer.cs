ca3567d8043311402e7f529ff4d622aa9db84f5e
Axwabo <axwabo@gmail.com>
2025-02-07 16:28:32 +0100

    basic landmark info

--- VolcanoSimulator/Rendering/SimulatorRenderer.cs
+++ VolcanoSimulator/Rendering/SimulatorRenderer.cs
@@ -13,6 +13,9 @@ public sealed class SimulatorRenderer
     private static readonly Coordinates Left = new(0, -1);
     private static readonly Coordinates Right = new(0, 1);
 
+    private const string Name = "Name: ";
+    private const string People = "People: ";
+
     private Coordinates _viewLocation = new(-Console.WindowHeight / 2, -Console.WindowWidth / 2);
 
     public SimulatorSession Session { get; }
@@ -32,29 +35,93 @@ public sealed class SimulatorRenderer
     private void Clear()
     {
         var viewport = Viewport;
+        var centerX = viewport.Width / 2;
+        var centerY = viewport.Height / 2;
         foreach (var landmark in Session.Landmarks)
+        {
             switch (landmark)
             {
                 case City city:
                     new CityRenderer(city).Clear(viewport);
                     break;
             }
+
+            if (!viewport.TryTransform(landmark.Location, out var screen) || screen.Longitude != centerX || screen.Latitude != centerY)
+                break;
+            Console.SetCursorPosition(screen.Longitude - 1, screen.Latitude);
+            Console.Write(' ');
+            Console.SetCursorPosition(screen.Longitude + 1, screen.Latitude);
+            Console.Write(' ');
+            ClearLandmarkInfo(landmark, viewport);
+        }
     }
+
+    private void ClearLandmarkInfo(LandmarkBase landmark, in ViewportRect viewport)
+    {
+        if (landmark is City city)
+        {
+            var length = Name.Length + city.Name.Length;
+            Console.SetCursorPosition(viewport.Width - length, 0);
+            for (var i = 0; i < length; i++)
+                Console.Write(' ');
+        }
+
+        if (landmark is IEvacuationLocation evacuationLocation)
+        {
+            var length = People.Length + IntLength(evacuationLocation.AccommodatedPeople);
+            Console.SetCursorPosition(viewport.Width - length, 1);
+            for (var i = 0; i < length; i++)
+                Console.Write(' ');
+        }
+    }
+
+    private static int IntLength(int value) => value == 0 ? 1 : (int) Math.Floor(Math.Log10(value)) + 1;
 
     private void Draw()
     {
         var viewport = Viewport;
+        var centerX = Console.WindowWidth / 2;
+        var centerY = Console.WindowHeight / 2;
         foreach (var landmark in Session.Landmarks)
+        {
             switch (landmark)
             {
                 case City city:
                     new CityRenderer(city).Draw(viewport);
+
                     break;
             }
 
-        Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            if (!viewport.TryTransform(landmark.Location, out var screen) || screen.Longitude != centerX || screen.Latitude != centerY)
+                break;
+            Console.SetCursorPosition(screen.Longitude - 1, screen.Latitude);
+            Console.Write('>');
+            Console.SetCursorPosition(screen.Longitude + 1, screen.Latitude);
+            Console.Write('<');
+            DrawLandmarkInfo(landmark, viewport);
+        }
+
+        Console.SetCursorPosition(centerX, centerY);
         Console.Write('+');
         _currentGui?.Draw();
+    }
+
+    private void DrawLandmarkInfo(LandmarkBase landmark, in ViewportRect viewport)
+    {
+        if (landmark is City city)
+        {
+            Console.SetCursorPosition(viewport.Width - Name.Length - city.Name.Length, 0);
+            Console.Write(Name);
+            Console.Write(city.Name);
+        }
+
+        if (landmark is IEvacuationLocation evacuationLocation)
+        {
+            var people = evacuationLocation.AccommodatedPeople.ToString();
+            Console.SetCursorPosition(viewport.Width - People.Length - people.Length, 1);
+            Console.Write(People);
+            Console.Write(people);
+        }
     }
 
     public void Move(Coordinates delta)
