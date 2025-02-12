5a9f03184e6792541f45bfca7033b3998bf1a507
Axwabo <axwabo@gmail.com>
2025-02-12 10:46:16 +0100

    pooled char array instead of ArraySegment in PlaceCityGui

--- VolcanoSimulator/Rendering/Gui/PlaceCityGui.cs
+++ VolcanoSimulator/Rendering/Gui/PlaceCityGui.cs
@@ -1,4 +1,5 @@
-﻿using VolcanoSimulator.Models;
+﻿using System.Buffers;
+using VolcanoSimulator.Models;
 
 namespace VolcanoSimulator.Rendering.Gui;
 
@@ -7,7 +8,9 @@ public sealed class PlaceCityGui : GuiBase
 
     private const string Title = "Place city:";
 
-    private ArraySegment<char> _name = new(new char[64], 0, 0);
+    private char[] _name = ArrayPool<char>.Shared.Rent(64);
+
+    private int _length;
 
     private bool _editingName = Console.CursorVisible = true;
 
@@ -31,27 +34,26 @@ public sealed class PlaceCityGui : GuiBase
             return key.Key.TryGetMovementDelta(out _) ? GuiInputResult.Passthrough : GuiInputResult.None;
         if (key.Key != ConsoleKey.Backspace)
         {
-            if (_name.Count == _name.Array!.Length)
+            if (_length == _name.Length)
                 return GuiInputResult.None;
             ClearName();
-            _name = new ArraySegment<char>(_name.Array, _name.Offset, _name.Count + 1);
-            _name.Array![_name.Count - 1] = key.KeyChar;
+            _name[_length++] = key.KeyChar;
             WriteName();
             return GuiInputResult.None;
         }
 
-        if (_name.Count == 0)
+        if (_length == 0)
             return GuiInputResult.None;
-        Console.SetCursorPosition(Console.WindowWidth - _name.Count, 1);
+        Console.SetCursorPosition(Console.WindowWidth - _length, 1);
         Console.Write(' ');
-        _name = _name[..^1];
+        _length--;
         WriteName();
         return GuiInputResult.None;
     }
 
     private GuiInputResult ToggleNameField(SimulatorRenderer renderer, in ConsoleKeyInfo key)
     {
-        if (_name.AsSpan().IsEmpty || !key.IsControl())
+        if (_length == 0 || !key.IsControl())
         {
             Console.CursorVisible = _editingName = !_editingName;
             return GuiInputResult.None;
@@ -61,18 +63,20 @@ public sealed class PlaceCityGui : GuiBase
         renderer.Session.Landmarks.Add(new City
         {
             Location = new Coordinates(viewport.Y + viewport.Height / 2, viewport.X + viewport.Width / 2),
-            Name = _name.AsSpan().ToString()
+            Name = _name.AsSpan()[.._length].ToString()
         });
         Console.CursorVisible = false;
         return GuiInputResult.Exit;
     }
 
-    private void ClearName() => Erase.TextRight(1, _name.Count);
+    private void ClearName() => Erase.TextRight(1, _length);
 
     private void WriteName()
     {
-        Render.TextRight(1, _name);
+        Render.TextRight(1, _name.AsSpan()[.._length]);
         Console.SetCursorPosition(Console.WindowWidth - 1, 1);
     }
+
+    ~PlaceCityGui() => ArrayPool<char>.Shared.Return(_name);
 
 }
