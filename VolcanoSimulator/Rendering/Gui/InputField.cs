18abbb9b3e84b8ba4721106d9246cbc0c75b2629
Axwabo <axwabo@gmail.com>
2025-02-12 11:01:47 +0100

    InputField

---
+++ VolcanoSimulator/Rendering/Gui/InputField.cs
@@ -0,0 +1,59 @@
+﻿using System.Buffers;
+
+namespace VolcanoSimulator.Rendering.Gui;
+
+public sealed class InputField
+{
+
+    private readonly int _row;
+    private readonly char[] _buffer;
+
+    public int Length { get; private set; }
+
+    public Span<char> Text => _buffer.AsSpan()[..Length];
+
+    public InputField(int row, int maxLength)
+    {
+        _row = row;
+        _buffer = ArrayPool<char>.Shared.Rent(maxLength);
+    }
+
+    public void Draw()
+    {
+        Render.TextRight(1, Text);
+        Console.SetCursorPosition(Console.WindowWidth - 1, 1);
+    }
+
+    public GuiInputResult ProcessInput(in ConsoleKeyInfo key)
+    {
+        if (key.Key == ConsoleKey.Enter)
+            return GuiInputResult.Exit;
+        if (key.Key != ConsoleKey.Backspace)
+        {
+            if (Length == _buffer.Length)
+                return GuiInputResult.None;
+            Clear();
+            _buffer[Length++] = key.KeyChar;
+            Write();
+            return GuiInputResult.None;
+        }
+
+        if (Length == 0)
+            return GuiInputResult.None;
+        Console.SetCursorPosition(Console.WindowWidth - Length--, 1);
+        Console.Write(' ');
+        Write();
+        return GuiInputResult.None;
+    }
+
+    private void Clear() => Erase.TextRight(_row, Length);
+
+    private void Write()
+    {
+        Render.TextRight(1, Text);
+        Console.SetCursorPosition(Console.WindowWidth - 1, 1);
+    }
+
+    ~InputField() => ArrayPool<char>.Shared.Return(_buffer);
+
+}
--- VolcanoSimulator/Rendering/Gui/PlaceCityGui.cs
+++ VolcanoSimulator/Rendering/Gui/PlaceCityGui.cs
@@ -1,5 +1,4 @@
-﻿using System.Buffers;
-using VolcanoSimulator.Models;
+﻿using VolcanoSimulator.Models;
 
 namespace VolcanoSimulator.Rendering.Gui;
 
@@ -8,75 +7,34 @@ public sealed class PlaceCityGui : GuiBase
 
     private const string Title = "Place city:";
 
-    private char[] _name = ArrayPool<char>.Shared.Rent(64);
-
-    private int _length;
-
-    private bool _editingName = Console.CursorVisible = true;
+    private readonly InputField _nameInput = new(1, 64);
 
     public override void Draw()
     {
+        Console.CursorVisible = true;
         Render.TextRight(0, Title);
-        WriteName();
+        _nameInput.Draw();
     }
 
     public override GuiInputResult ProcessInput(SimulatorRenderer renderer, in ConsoleKeyInfo key)
     {
-        if (key.Key == ConsoleKey.Enter)
-            return ToggleNameField(renderer, key);
         if (key.Key == ConsoleKey.Escape)
         {
             Console.CursorVisible = false;
             return GuiInputResult.Exit;
         }
 
-        if (!_editingName)
-            return key.Key.TryGetMovementDelta(out _) ? GuiInputResult.Passthrough : GuiInputResult.None;
-        if (key.Key != ConsoleKey.Backspace)
-        {
-            if (_length == _name.Length)
-                return GuiInputResult.None;
-            ClearName();
-            _name[_length++] = key.KeyChar;
-            WriteName();
+        var result = _nameInput.ProcessInput(key);
+        if (result != GuiInputResult.Exit)
             return GuiInputResult.None;
-        }
-
-        if (_length == 0)
-            return GuiInputResult.None;
-        Console.SetCursorPosition(Console.WindowWidth - _length, 1);
-        Console.Write(' ');
-        _length--;
-        WriteName();
-        return GuiInputResult.None;
-    }
-
-    private GuiInputResult ToggleNameField(SimulatorRenderer renderer, in ConsoleKeyInfo key)
-    {
-        if (_length == 0 || !key.IsControl())
-        {
-            Console.CursorVisible = _editingName = !_editingName;
-            return GuiInputResult.None;
-        }
-
         var viewport = renderer.Viewport;
         renderer.Session.Landmarks.Add(new City
         {
             Location = new Coordinates(viewport.Y + viewport.Height / 2, viewport.X + viewport.Width / 2),
-            Name = _name.AsSpan()[.._length].ToString()
+            Name = _nameInput.Text.ToString()
         });
         Console.CursorVisible = false;
         return GuiInputResult.Exit;
     }
-
-    private void ClearName() => Erase.TextRight(1, _length);
-
-    private void WriteName()
-    {
-        Render.TextRight(1, _name.AsSpan()[.._length]);
-        Console.SetCursorPosition(Console.WindowWidth - 1, 1);
-    }
-
-    ~PlaceCityGui() => ArrayPool<char>.Shared.Return(_name);
 
 }
