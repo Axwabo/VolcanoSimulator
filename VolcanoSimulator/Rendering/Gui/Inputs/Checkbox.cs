8706c60202d7f00e0ffcce8123b532af084f4f98
Axwabo <axwabo@gmail.com>
2025-02-16 17:22:10 +0100

    Checkbox

---
+++ VolcanoSimulator/Rendering/Gui/Inputs/Checkbox.cs
@@ -0,0 +1,41 @@
+﻿namespace VolcanoSimulator.Rendering.Gui.Inputs;
+
+public sealed class Checkbox : IInputField
+{
+
+    private const string CheckedText = "[X]";
+    private const string UncheckedText = "[ ]";
+
+    public int Row { get; }
+    public bool Checked { get; private set; }
+    private string Text => Checked ? CheckedText : UncheckedText;
+
+    public Checkbox(int row, bool @checked)
+    {
+        Row = row;
+        Checked = @checked;
+    }
+
+    public void Draw(bool active)
+    {
+        Render.TextRight(Row, Text);
+        if (active)
+            Console.SetCursorPosition(Console.WindowWidth - 2, Row);
+    }
+
+    public GuiInputResult ProcessInput(in ConsoleKeyInfo key)
+    {
+        switch (key.Key)
+        {
+            case ConsoleKey.Spacebar:
+                Checked = !Checked;
+                Draw(true);
+                return GuiInputResult.None;
+            case ConsoleKey.Enter or ConsoleKey.Escape:
+                return GuiInputResult.Exit;
+            default:
+                return GuiInputResult.None;
+        }
+    }
+
+}
