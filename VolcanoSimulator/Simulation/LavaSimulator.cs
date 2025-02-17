04997072c237da62a40c4f88312560d94d244132
Axwabo <axwabo@gmail.com>
2025-02-17 20:31:11 +0100

    claim lives in LavaSimulator

--- VolcanoSimulator/Simulation/LavaSimulator.cs
+++ VolcanoSimulator/Simulation/LavaSimulator.cs
@@ -1,10 +1,13 @@
 ﻿using UnitsNet;
 using VolcanoSimulator.Models;
+using VolcanoSimulator.Rendering.Renderers;
 
 namespace VolcanoSimulator.Simulation;
 
 public sealed class LavaSimulator : EruptedMaterialSimulator<Lava>
 {
+
+    private readonly List<City> _cities = [];
 
     public static Speed FlowRate { get; } = Speed.FromKilometersPerHour(8); // TODO: interpolate using height
 
@@ -15,6 +18,23 @@ public sealed class LavaSimulator : EruptedMaterialSimulator<Lava>
         if (Material.CanFlow)
             Material.Flow(FlowRate * time);
         Material.Cool(CoolingRate * time);
+        if (!Material.HasDecayed)
+            ClaimLives(session);
+    }
+
+    private void ClaimLives(SimulatorSession session)
+    {
+        using var handler = new UniformCasualtyHandler(1, _cities, session);
+        var (startX, startY) = Material.Location;
+        var endX = startX + (int) (Material.Length / PositionedRenderer.PixelSize);
+        var endY = startY + (int) (Material.Width / PositionedRenderer.PixelSize);
+        if (startX > endX)
+            (startX, endX) = (endX, startX);
+        if (startY > endY)
+            (startY, endY) = (endY, startY);
+        for (var y = startY; y <= endY; y++)
+        for (var x = startX; x <= endX; x++)
+            handler.Process(new Coordinates(y, x));
     }
 
 }
---
+++ VolcanoSimulator/Simulation/UniformCasualtyHandler.cs
@@ -0,0 +1,32 @@
+﻿using VolcanoSimulator.Models;
+
+namespace VolcanoSimulator.Simulation;
+
+public readonly ref struct UniformCasualtyHandler : IDisposable
+{
+
+    private readonly double _rate;
+    private readonly List<City> _cityCache;
+
+    public UniformCasualtyHandler(double rate, List<City> cityCache, SimulatorSession session)
+    {
+        _rate = rate;
+        _cityCache = cityCache;
+        _cityCache.AddRange(session.Landmarks.OfType<City>());
+    }
+
+    public void Process(in Coordinates location)
+    {
+        for (var i = 0; i < _cityCache.Count; i++)
+        {
+            var city = _cityCache[i];
+            if (city.Location != location)
+                continue;
+            city.KillPercentage(_rate);
+            _cityCache.RemoveAt(i--);
+        }
+    }
+
+    public void Dispose() => _cityCache.Clear();
+
+}
