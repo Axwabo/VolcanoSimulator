1a729931b7b93e0c671634ee8eb0f68e178ed8b7
Axwabo <axwabo@gmail.com>
2025-02-17 18:46:52 +0100

    spew multiple lava instances on eruption

--- VolcanoSimulator/Models/Volcano.cs
+++ VolcanoSimulator/Models/Volcano.cs
@@ -5,6 +5,11 @@ namespace VolcanoSimulator.Models;
 public sealed class Volcano : NamedLandmark
 {
 
+    private static Temperature RandomLavaTemperature => Temperature.FromDegreesCelsius(Random.Shared.Next(
+        (int) Lava.MinInitialTemperature.DegreesCelsius,
+        (int) Lava.MaxInitialTemperature.DegreesCelsius
+    ));
+
     private static Density InitialCloudDensity(VolcanicExplosivityIndex index) => Density.FromKilogramsPerCubicMeter(1 - 1d / index.Index);
 
     private readonly List<EruptedMaterialBase> _eruptedMaterial = [];
@@ -23,28 +28,39 @@ public sealed class Volcano : NamedLandmark
                 Strength = Force.FromKilonewtons(1)
             };
         var totalEjecta = ExplosivityIndex.EjectaVolume;
-        var lavaVolume = totalEjecta * 0.01d;
+        var lavaVolume = totalEjecta * 0.01;
         _eruptedMaterial.Add(new AshCloud
         {
             Location = Location,
             Mass = InitialCloudDensity(ExplosivityIndex) * (totalEjecta - lavaVolume)
         });
-        _eruptedMaterial.Add(new Lava
-        {
-            Location = Location,
-            InitialTemperature = Temperature.FromDegreesCelsius(Random.Shared.Next(
-                (int) Lava.MinInitialTemperature.DegreesCelsius,
-                (int) Lava.MaxInitialTemperature.DegreesCelsius
-            )),
-            FlowAngle = Angle.FromDegrees(Random.Shared.NextDouble() * 360),
-            Volume = lavaVolume
-        });
+        SpewLava(lavaVolume);
         return new Earthquake
         {
             Duration = TimeSpan.FromMinutes(Random.Shared.Next(ExplosivityIndex.Index * 2, ExplosivityIndex.Index * 10)),
             Epicenter = Location,
             Strength = Force.FromNewtons((ExplosivityIndex.Index + 1) * 100)
         };
+    }
+
+    private void SpewLava(Volume volume)
+    {
+        var count = Math.Clamp(Random.Shared.Next(3, 6), 0, ExplosivityIndex.Index);
+        var volumePerInstance = volume / count;
+        var volumeRandomness = volumePerInstance * 0.001;
+        var angleDeltaPerInstance = 360d / count;
+        var angle = Random.Shared.NextDouble() * 360;
+        for (var i = 0; i < count; i++)
+        {
+            _eruptedMaterial.Add(new Lava
+            {
+                Location = Location,
+                InitialTemperature = RandomLavaTemperature,
+                FlowAngle = Angle.FromDegrees(angle),
+                Volume = volumePerInstance + (Random.Shared.NextDouble() * 2 - 1) * volumeRandomness
+            });
+            angle += angleDeltaPerInstance + Random.Shared.Next(-10, 10);
+        }
     }
 
     public void ClearDecayedMaterial() => _eruptedMaterial.RemoveAll(e => e.HasDecayed);
