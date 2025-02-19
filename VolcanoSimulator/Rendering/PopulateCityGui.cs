cdb183ce58c0c96850acfb594d496e0b6acb9aa8
Axwabo <axwabo@gmail.com>
2025-02-19 09:34:13 +0100

    exit from PopulateCityGui if input is 0

--- VolcanoSimulator/Rendering/PopulateCityGui.cs
+++ VolcanoSimulator/Rendering/PopulateCityGui.cs
@@ -25,12 +25,14 @@ public sealed class PopulateCityGui : GuiBase, IActionModeModifier
 
     public override GuiInputResult ProcessInput(SimulatorRenderer renderer, in ConsoleKeyInfo key)
     {
-        if (key.Key != ConsoleKey.Enter || _input.Input.Value == 0)
+        if (key.Key != ConsoleKey.Enter)
         {
             _input.ProcessInput(key);
             return GuiInputResult.None;
         }
 
+        if (_input.Input.Value == 0)
+            return GuiInputResult.Exit;
         _city.Shelter(_input.Input.Value);
         Console.CursorVisible = false;
         return GuiInputResult.Exit;
